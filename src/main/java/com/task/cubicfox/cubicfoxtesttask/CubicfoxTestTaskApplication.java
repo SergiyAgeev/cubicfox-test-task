e6eb7dbdf11df9e4e6e1fd28d6874d1054b5d854
SergiyAgeev <sergiyageev@gmail.com>
2020-03-10 17:30:59 +0200

    initialise spring boot project

---
+++ src/main/java/com/task/cubicfox/cubicfoxtesttask/CubicfoxTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.task.cubicfox.cubicfoxtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubicfoxTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubicfoxTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/cubicfox/cubicfoxtesttask/CubicfoxTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.cubicfox.cubicfoxtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubicfoxTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
